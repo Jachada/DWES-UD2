efea295e3c4dda28d757c328b148aae99b6687db
Jachada <carmen.m.ru.pa@gmail.com>
2021-09-27 17:21:09 +0200

    Ejercicio 1 de la presentación UD2. Introducción a PHP - Parte 2

---
+++ ejercicio1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <?php
+        $nombre = "Carmen Mª Rufo Palomo";
+        $poblacion = "Sevilla";
+        $edad = 22;
+
+        printf("Soy %s, tengo %d años de edad y vivo en %s", $nombre, $edad, $poblacion);
+        echo ("<br>");
+        echo("Soy $nombre, tengo $edad años de edad y vivo en $poblacion");
+    ?>
+</body>
+</html>
